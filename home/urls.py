089296c072524a6ed089059525e9c3fa14f16263
Vansh <vanshpthakkar@gmail.com>
2024-02-17 19:31:04 +0530

    Dynamic form created

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path,include
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="Index")
+    path('', views.index, name="Index"),
+    path('fillForm/', views.fillForm, name="Fill Form")
 ]
